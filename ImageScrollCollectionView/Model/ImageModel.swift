dc57787fd902c356e8bf3757e43e20f19acd1a83
Aleksandr Aniskin <aleksandr.nskn@yandex.ru>
2021-08-02 17:05:06 +0300

    0.5

--- ImageScrollCollectionView/Model/ImageModel.swift
+++ ImageScrollCollectionView/Model/ImageModel.swift
@@ -13,10 +13,10 @@ struct ImageModel {
     static func setImages() -> [ImageModel] {
         let image1 = ImageModel(imageURL: "image/cache/catalog/p/8024/9t0a8178-h_1_630x840.jpg")
         let image2 = ImageModel(imageURL: "image/cache/catalog/p/8109/kiro0348-h_1_630x840.jpg")
-        let image3 = ImageModel(imageURL: "image/cache/catalog/p/8047/9t0a4451-h_1_630x840.jp")
+        let image3 = ImageModel(imageURL: "image/cache/catalog/p/8047/9t0a4451-h_1_630x840.jpg")
         let image4 = ImageModel(imageURL: "image/cache/catalog/p/8024/9t0a8178-h_1_630x840.jpg")
         let image5 = ImageModel(imageURL: "image/cache/catalog/p/8024/9t0a8178-h_1_630x840.jpg")
-        let image6 = ImageModel(imageURL: "image/image/cache/catalog/p/8024/9t0a8178-h_1_630x840.jpg")
+        let image6 = ImageModel(imageURL: "image/cache/catalog/p/8109/kiro0348-h_1_630x840.jpg")
         
         let imageArr: [ImageModel] = [image1, image2, image3, image4, image5, image6]
         
